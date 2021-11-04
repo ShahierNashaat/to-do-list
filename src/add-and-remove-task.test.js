65d6efd98e2a98fbbc270c3d07ab843c470a5b88
Shahier Nashaat <shahiern@yahoo.com>
2021-11-04 14:00:05 +0200

    Remove edit, complete and clear from the branck

--- src/add-and-remove-task.test.js
+++ src/add-and-remove-task.test.js
@@ -1,6 +1,5 @@
 import localStorageController from './__mocks__/localStorage-controller';
 import taskController from './add-and-remove-task';
-import taskStatusChange from './task_status_change';
 
 jest.mock('./localStorage-controller');
 
@@ -11,22 +10,5 @@ describe('to do list testing', () => {
   });
   test('Delete task', () => {
     expect(taskController.deleteTask(0)).toHaveLength(2);
-  });
-  test('Edit task', () => {
-    expect(taskController.editTask('task1', 0)).toBe('task1');
-  });
-  test('Clear completed tasks', () => {
-    const tasksArr = taskController.clearCompletedTasks();
-    const filteredArr = tasksArr.filter((task) => task.completed);
-    expect(filteredArr).toHaveLength(0);
-  });
-  test('Complete task true or false', () => {
-    let tasksArr = taskStatusChange(0, true);
-    let filteredArr = tasksArr.filter((task) => task.completed);
-    expect(filteredArr).toHaveLength(1);
-
-    tasksArr = taskStatusChange(0, false);
-    filteredArr = tasksArr.filter((task) => task.completed);
-    expect(filteredArr).toHaveLength(0);
   });
 });
